9686919417efe5fd938d06a933c1a65519b0d734
emmagmazel <98437069+emmagmazel@users.noreply.github.com>
2022-01-25 20:35:11 -0500

    JavaScript file added

---
+++ script.js
@@ -0,0 +1,52 @@
+// Task 1
+
+//Declare three variables, a and b and c.
+let a, b,c;
+
+// If a has value 3, b has value 5, and c doesn't have a value, alert the following expressions ( one after another ). Alert popup box has to show the following content for each expression (concatenate everything in one long expression using "\n" to make new lines):
+a = 3;
+b = 5; 
+
+let output = '';
+output += 'let a = 3\n';
+output += 'let b = 5\n';
+output += 'let c = undefined\n';
+output += '-------------------\n';
+output += 'a + b = ' + (a + b) + '\n';
+output += 'a - b = ' + (a - b) + '\n';
+output += 'a * b = ' + (a * b) + '\n';
+output += 'a / b = ' + (a / b) + '\n';
+output += 'a % b = ' + (a % b) + '\n';
+output += '(a += b): ' + (a += b) + '\n';
+output += '(a -= b): ' + (a -= b) + '\n';
+output += '(a /= b): ' + (a /= b) + '\n';
+output += '(a %= b): ' + (a %= b) + '\n';
+output += '(a == b): ' + (a == b) + '\n';
+output += '(a != b): ' + (a != b) + '\n';
+output += '(a > b): ' + (a > b) + '\n';
+output += '(a < b): ' + (a < b) + '\n';
+output += '(!a && !c): ' + (!a && !c ) + '\n';
+output += '(!a || !c): ' + (!a || !c ) + '\n';
+
+
+alert(output);
+
+
+
+
+// Notation of JS object:
+let person = {
+    sentence:'My name is',
+    first_name: 'Emma',
+    last_name: 'Gmazel.',
+    email: 'gmaz0001@algonquinlive.com.'
+};
+
+console.log(person.first_name); // Emma
+console.log(person.last_name); // Gmazel
+console.log(person.email); // gmaz0001@algonquinlive.com
+console.log(person);
+
+alert (person);
+
+ 
